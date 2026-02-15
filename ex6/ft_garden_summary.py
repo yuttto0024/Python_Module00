8dad096d43c1c5d2910f8c8cd34402c14189995f
yuttto0024 <yuto0024@outlook.com>
2026-02-15 16:28:20 +0900

    ex6

---
+++ ex6/ft_garden_summary.py
@@ -0,0 +1,9 @@
+def ft_garden_summary() -> None:
+    """
+    Merge garden data and takes input of each datas
+    """
+    garden_name = input("Enter garden name: ")
+    plants_number = input("Enter number of plants: ")
+    print(f"Garden: {garden_name}")
+    print(f"Plants: {plants_number}")
+    print("Status: Growing well!")
